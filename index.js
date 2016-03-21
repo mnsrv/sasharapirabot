356c4a828b6fc244044030c06940614663cd6193
sashamjolnir <mansurovs@rambler.ru>
2016-03-22 01:32:15 +0300

    add vk

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var TelegramBot = require('node-telegram-bot-api');
+var request = require('request');
 
 var TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
@@ -6,8 +7,18 @@ var TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 var bot = new TelegramBot(TOKEN, {polling: true});
 
 // Any kind of message
-bot.on('message', function (msg) {
+bot.onText(/(music|музыка)/, function (msg) {
     var fromId = msg.from.id;
-    var text = 'hello world';
-    bot.sendMessage(fromId, text);
+    var text = 'сейчас не играет никакая музыка';
+    request('https://api.vk.com/method/users.get?user_ids=7149276&fields=status', function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var info = JSON.parse(body);
+            var vk_response = info.response[0];
+            if (vk_response.status_audio) {
+                text = 'сейчас играет: ';
+                text = text + vk_response.status_audio.artist + ' – ' + vk_response.status_audio.title;
+            }
+            bot.sendMessage(fromId, text);
+        }
+    });
 });
