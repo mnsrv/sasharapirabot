98da49b310db980c73cbe4923f10b67a7a528880
Aleksandr Mansurov <am@idaproject.com>
2016-04-28 13:37:56 +0300

    😎😎😎 ADD LAST.FM API 😎😎😎

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
 var bot = new TelegramBot(TOKEN, {polling: true});
 
-bot.onText(/(music|музыка)/, function (msg) {
+console.log('hello');
+
+bot.onText(/(music vk|музыка вк|vk music|vk)/, function (msg) {
     var chatId = msg.chat.id;
     var text = 'сейчас не играет никакая музыка';
     request('https://api.vk.com/method/users.get?user_ids=7149276&fields=status', function (error, response, body) {
@@ -19,4 +21,26 @@ bot.onText(/(music|музыка)/, function (msg) {
             bot.sendMessage(chatId, text);
         }
     });
+});
+
+bot.onText(/(music spotify|музыка спотифай|спотифай|spotify music|spotify|шо за трек|шозатрек)/, function(msg) {
+    var chatId = msg.chat.id;
+    var text = 'сейчас не играет никакая музыка';
+    request(
+        {
+            url: 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=iamseventeen&api_key=531f66f07ff0cc0c50172812007a8679&format=json',
+            method: 'GET',
+        },
+        function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var info = JSON.parse(body);
+                var track = info.recenttracks.track[0];
+                var artist = track.artist['#text'];
+                var name = track.name;
+
+                var output = '🎧Сейчас играет: ' + artist + ' – ' + name;
+                bot.sendMessage(chatId, output);
+            }
+        }
+    );
 });
